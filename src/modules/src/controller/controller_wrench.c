e2073d455abcdc39c3e105ba18dd9735a713fd37
Logan Dihel <logan.dihel@gmail.com>
2025-03-26 11:21:16 -0400

    max thrust is 65000 not 65535

--- src/modules/src/controller/controller_wrench.c
+++ src/modules/src/controller/controller_wrench.c
@@ -26,12 +26,14 @@ void controllerWrench(control_t *control, const setpoint_t *setpoint,
                                          const state_t *state,
                                          const stabilizerStep_t stabilizerStep)
 {
-  float commandedThrustPercent = setpoint->thrust / 65535.0f;
+  float normalizedThrust = setpoint->thrust / 65000.0f; // max thrust is 65000, NOT 65535
+  float thrust_g = normalizedThrust * 650.0f; // thrust in grams
+  float thrust_N = thrust_g * 0.00980665f; // thrust in N
 
   control->controlMode = controlModeWrench;
   control->Fx = 0.0f; // N
   control->Fy = 0.0f; // N
-  control->Fz = commandedThrustPercent * 650.0f * 0.009866f; // convert to N using grams as intermediate step
+  control->Fz = thrust_N; // N
   control->Tx = 0.0f; // Nm
   control->Ty = 0.0f; // Nm
   control->Tz = 0.0f; // Nm
