fb7594a87f781c35d042c0ad20d15c7fa49b2e4f
Logan Dihel <logan.dihel@gmail.com>
2026-01-24 09:41:33 -0500

    adding gemus as a separate platform

--- src/platform/interface/platform_defaults.h
+++ src/platform/interface/platform_defaults.h
@@ -49,6 +49,9 @@
 #ifdef CONFIG_PLATFORM_BICOPTER
     #include "platform_defaults_bicopter.h"
 #endif
+#ifdef CONFIG_PLATFORM_GEMUS
+    #include "platform_defaults_gemus.h"
+#endif
 
 // Drone physical constants
 // TODO provide those in each config. Those are values of cf2 platform
